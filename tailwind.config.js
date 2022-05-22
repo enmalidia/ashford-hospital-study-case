e45e0fceed3f656acf315ca1b5e433ec6bd2af46
Enma Lidia Muñoz García <enmalidia88@gmail.com>
2022-05-21 23:25:11 -0400

    style: :sparkles: Configuring the style theme
    setting up: color palette, custom fonts, dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,30 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
+  purge: {
+    enabled: false
+  }, 
+  darkMode: 'class', 
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Futura Std': ['Futura Std','sans-serif'],
+        'Untitled Sans': ['Untitled Sans','sans-serif'],
+      },
+      colors:{
+        primary: '#3EB1C8',
+        secondary:'#002554',
+        lightGray: '#8994A7',
+        darkGray: '#27364B'
+      },
+      screens:{
+        'xs': '360px',
+        ...defaultTheme.screens
+      },
+    },
   },
   plugins: [],
 }
