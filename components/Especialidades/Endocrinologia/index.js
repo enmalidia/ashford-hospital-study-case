62dc693f2a537c76546fc4233a63577293e6704d
Enma Lidia Muñoz García <enmalidia88@gmail.com>
2022-05-29 22:31:19 -0400

    feat: :sparkles: Specialties page: coding the markup for the Endocrinologia section

--- components/Especialidades/Endocrinologia/index.js
+++ components/Especialidades/Endocrinologia/index.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import { FiArrowRight, FiPhone, FiClock } from "react-icons/fi";
+import { BsGeo } from "react-icons/bs";
+import data from './data.json'
 
 const Endocrinologia = () => {
   return (
-    <div>Endocrinología</div>
+    <div className='container mx-auto'>
+      <h5>especialidad</h5>
+      <h1 className='text-darkGray'>Endocrinología</h1>
+      <div className='md:flex'>
+        <div className='w-full md:w-2/4'>
+          <p>El centro endocrinológico cuenta con tecnología innovadora, personal de vasta experiencia y un grupo de médicos especializados en endocrinología. Atendemos condiciones como hipotiroidismo, hipertiroidismo, páncreas endocrino, diabetes, entre otros.</p>
+        </div>
+        <div className='w-full md:w-2/4'>         
+          {
+            React.Children.toArray(
+              data.map(item => ( 
+                <div className='flex'>
+                  <FiArrowRight className='text-primary'/>
+                  <p>{item.item}</p>                      
+                </div>                                                                                                 
+              ))
+            )
+          }
+        </div>
+      </div>                   
+    </div>
   )
 }
 
--- components/Especialidades/MedicinaDeportiva/index.js
+++ components/Especialidades/MedicinaDeportiva/index.js
@@ -6,26 +6,26 @@ import data from './data.json'
 const MedicinaDeportiva = () => {
   return (
     <div className='container mx-auto'>
-    <h5>especialidad</h5>
-    <h1 className='text-darkGray'>Medicina deportiva</h1>
-    <div className='md:flex'>
-      <div className='w-full md:w-2/4'>
-        <p>La medicina deportiva es la especialidad médica que estudia los efectos del ejercicio, deporte y la actividad física en el cuerpo humano. Nuestro equipo se encarga de preparar y optimizar la condición del cuerpo a través del cuidado, asesoramiento, diagnóstico y tratamiento necesario para practicar deportes de forma segura.</p>
-      </div>
-      <div className='w-full md:w-2/4'>         
-        {
-          React.Children.toArray(
-            data.map(item => ( 
+      <h5>especialidad</h5>
+      <h1 className='text-darkGray'>Medicina deportiva</h1>
+      <div className='md:flex'>
+        <div className='w-full md:w-2/4'>
+          <p>La medicina deportiva es la especialidad médica que estudia los efectos del ejercicio, deporte y la actividad física en el cuerpo humano. Nuestro equipo se encarga de preparar y optimizar la condición del cuerpo a través del cuidado, asesoramiento, diagnóstico y tratamiento necesario para practicar deportes de forma segura.</p>
+        </div>
+        <div className='w-full md:w-2/4'>         
+          {
+            React.Children.toArray(
+              data.map(item => ( 
                 <div className='flex'>
                   <FiArrowRight className='text-primary'/>
                   <p>{item.item}</p>                      
                 </div>                                                                                                 
-            ))
-          )
-        }
-      </div>
-    </div> 
-    <p className='text-primary'>Información, ubicación y horarios</p> 
+              ))
+            )
+          }
+        </div>
+      </div> 
+      <p className='text-primary'>Información, ubicación y horarios</p> 
       <div className='flex'>
         <div className='flex'>
           <FiPhone className='text-primary mr-2'/>
