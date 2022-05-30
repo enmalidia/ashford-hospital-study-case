fd12d767dd6140119c9194612c1730d1d68ffa0f
Enma Lidia Muñoz García <enmalidia88@gmail.com>
2022-05-29 22:40:29 -0400

    feat: :sparkles: Specialties page: coding the markup for the Cirugia oncologica section

--- components/Especialidades/CirugiaOncologica/index.js
+++ components/Especialidades/CirugiaOncologica/index.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const CirugiaOncologica = () => {
   return (
-    <div>CirugiaOncologica</div>
+    <div className='container mx-auto'>
+      <h5>especialidad</h5>
+      <h1 className='text-darkGray'>Cirugía oncológica</h1>      
+      <p>a cirugía oncológica es la especialidad médica enfocada en la prevención, diagnóstico, tratamiento quirúrgico y seguimiento de los pacientes con tumores, sea un cáncer benigno o maligno. Nuestros oncólogos están a la vanguardia de los tratamientos y terapias innovadoras para manejar cada caso de forma individualizada y precisa y así lograr resultados efectivos. Estamos enfocados a proveer a cada paciente un servicio de la mayor calidad, empatía y humanidad.</p>
+    </div>
   )
 }
 
