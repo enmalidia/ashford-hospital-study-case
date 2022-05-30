68ab69b51857657ca981a2123e75bcd24d204981
Enma Lidia Muñoz García <enmalidia88@gmail.com>
2022-05-29 22:52:14 -0400

    feat: :sparkles: Specialties section: coding the markup for the Pediatria section

--- components/Especialidades/Obstetricia/index.js
+++ components/Especialidades/Obstetricia/index.js
@@ -4,10 +4,10 @@ const Obstetricia = () => {
   return (
     <div className='mx-auto md:flex'>      
       <div className='w-full md:w-7/12'>
-        <h5>especialidades</h5>
-        <h1 className='text-darkGray'>Obstetricia, ginecología y ginecología oncológica</h1>
+        <h5>especialidad</h5>
+        <h1 className='text-darkGray'>Pediatría</h1>
         <p className='text-primary'>Cuidamos por la salud de la mujer en todo momento</p>
-        <p>Nuestra facultad médica está integrada por obstetras, ginecólogos, perinatólogos y ginecólogos oncólogos de vasta experiencia preparados para prevenir y tratar cualquier condición ginecológica. Nuestros obstetras y perinatólogos están preparados para atender partos, así como condiciones de alto riesgo en el embarazo. Anualmente, el Hospital Ashford es el hospital privado de Puerto Rico donde nacen más bebés y la primera institución hospitalaria en Puerto Rico que ha sido certificada como “Hospital Amigo del Niño.</p>
+        <p>En el Hospital Ashford nos encantan los niños y tenemos el Departamento de Pediatría con el más avanzado equipo técnico y el respaldo del mejor equipo de pediatras en toda la Isla. Nuestro compromiso con las familias es proveer respuestas y tratamientos que les provean paz mental para que sus niños siempre estén saludables y felices. El Ashford también cuenta con una amplia sala de emergencias pediátricas para ofrecerte un servicio de calidad y excelencia.</p>
       </div>
       <div className='w-full md:w-5/12'>
         <img src="/images/especialties/spe1.png" alt="" />
--- components/Especialidades/Pediatria/index.js
+++ components/Especialidades/Pediatria/index.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { FiArrowRight, FiPhone, FiClock } from "react-icons/fi";
 
 const Pediatria = () => {
   return (
-    <div>Pediatria</div>
+    <div className='mx-auto md:flex'>      
+      <div className='w-full md:w-7/12 bg-grayBackground'>
+        <h5>especialidad</h5>
+        <h1 className='text-darkGray'>Pediatría</h1>        
+        <p>En el Hospital Ashford nos encantan los niños y tenemos el Departamento de Pediatría con el más avanzado equipo técnico y el respaldo del mejor equipo de pediatras en toda la Isla. Nuestro compromiso con las familias es proveer respuestas y tratamientos que les provean paz mental para que sus niños siempre estén saludables y felices. El Ashford también cuenta con una amplia sala de emergencias pediátricas para ofrecerte un servicio de calidad y excelencia.</p>
+        <p className='text-primary'>Información, ubicación y horarios</p> 
+        <div className='flex'>
+          <div className='flex'>
+            <FiPhone className='text-primary mr-2'/>
+            <p className='text-xs'>(787) 721-2160, ext. 2103 o 2104</p>
+          </div>        
+        </div> 
+      </div>
+      <div className='w-full md:w-5/12'>
+        <img src="/images/especialties/spe10.png" alt="" />
+      </div>
+    </div>
   )
 }
 
